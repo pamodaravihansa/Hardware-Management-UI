8b7fb0c92da1533cbd3769036c4efb6cb8b0dc5b
Pamoda Ravihansa <pamoda.ravihansa@gmail.com>
2024-03-28 22:41:51 +0530

    top nav bar developed

--- src/Components/toptitlebar.js
+++ src/Components/toptitlebar.js
@@ -1,15 +1,16 @@
 import '../CSS/style.css';
+import profileImage from '../assets/images/profile_image.png';
 
 export default function TopTitleBar() {
     return (
         <div id="top-title-bar">
             <h2>Hardware Store Management System</h2>
             <div id="profile-section">
-                <div>
+                <div id="profile-names">
                     <span id="yourname">Your Name</span>
                     <span id="admin">Admin</span>
                 </div>
-                <img></img>
+                <img src= {profileImage}></img>
             </div>
         </div>
         
