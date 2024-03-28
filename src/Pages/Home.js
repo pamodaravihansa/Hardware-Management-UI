ecd9ff781abc5a7d40b8b9e49bdb59a2049ec18a
Pamoda Ravihansa <pamoda.ravihansa@gmail.com>
2024-03-29 00:57:37 +0530

    category buttons css done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import '../CSS/style.css';
 
 import SideNavBar from '../Components/sidenavbar.js'
 import TopTitleBar from '../Components/toptitlebar.js';
+import CategoryButtons from '../Components/categorybuttons.js';
 
 export default function Home() {
     return (
@@ -10,7 +11,8 @@ export default function Home() {
             <div id="right-side">
                 <TopTitleBar />
                 <div id="homepage-content">
-
+                    <span id="choose-title">Choose Category</span>
+                    <CategoryButtons/>
                 </div>
             </div>
         </div>
