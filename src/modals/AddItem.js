14f0db4a50bfd1651dc984adf5faca827ef9ee14
Pamoda Ravihansa <pamoda.ravihansa@gmail.com>
2024-04-03 21:58:29 +0530

    create modal files

---
+++ src/modals/AddItem.js
@@ -0,0 +1,12 @@
+import '../CSS/style.scss';
+
+export default function AddItemModal() {
+    return (
+        <div>
+            <div className='add-modal-topsection'></div>
+            <div className='add-modal-inputs'></div>
+            <div className='add-modal-buttons-section'></div>
+        </div>
+
+    );
+}
